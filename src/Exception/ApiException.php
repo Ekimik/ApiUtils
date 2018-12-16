9237a0b8b39c4ff30efcdfddd25d7c99137effe0
Jan Jíša <j.jisa@seznam.cz>
2018-12-16 18:46:22 +0100

    add message from prev exception to errors in api exception

--- src/Exception/ApiException.php
+++ src/Exception/ApiException.php
@@ -2,9 +2,20 @@
 
 namespace Ekimik\ApiUtils\Exception;
 
+use Throwable;
+
 class ApiException extends \Exception {
 
     protected $errors = [];
+
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+
+        if (!empty($previous)) {
+            $e = ['message' => $previous->getMessage()];
+            $this->setErrors([$e]);
+        }
+    }
 
     public function getErrors(): array {
         return $this->errors;
